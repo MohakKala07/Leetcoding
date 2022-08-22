4879e38fd094193684a38215435f9aab8b212bcc
MohakKala07 <84536833+MohakKala07@users.noreply.github.com>
2022-08-22 22:55:13 +0530

    Time: 358 ms (55.04%), Space: 117.9 MB (59.47%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -11,19 +11,40 @@
 class Solution 
 {
 public:
-    ListNode* temp;
     bool isPalindrome(ListNode* head) 
     {
-        temp = head;
-        return check(head);
+        if(head==NULL||head->next==NULL)
+            return true;
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast->next!=NULL&&fast->next->next!=NULL)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        slow->next=reverseList(slow->next);
+        slow=slow->next;
+        while(slow!=NULL)
+        {
+            if(head->val!=slow->val)
+                return false;
+            head=head->next;
+            slow=slow->next;
+        }
+        return true;
     }
     
-    bool check(ListNode* p) 
+    ListNode* reverseList(ListNode* head) 
     {
-        if (NULL == p) 
-            return true;
-        bool isPal = check(p->next) & (temp->val == p->val);
-        temp = temp->next;
-        return isPal;
+        ListNode* pre=NULL;
+        ListNode* next=NULL;
+        while(head!=NULL)
+        {
+            next=head->next;
+            head->next=pre;
+            pre=head;
+            head=next;
+        }
+        return pre;
     }
 };
