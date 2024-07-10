1f051bfd8205bdd774260dd1defa60a735ec63f9
Mohak Kala <84536833+MohakKala07@users.noreply.github.com>
2024-07-10 19:05:18 +0530

    Time: 257 ms (61.35%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,3 +1,5 @@
 # Write your MySQL query statement below
 
-SELECT MAX(Salary) AS SecondHighestSalary FROM Employee WHERE Salary <(SELECT MAX(Salary) FROM Employee)
+SELECT IFNULL(
+(SELECT DISTINCT Salary from Employee ORDER BY Salary DESC LIMIT 1 OFFSET 1),NULL)
+ AS SecondHighestSalary
